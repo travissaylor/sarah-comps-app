90bdca1b857041770a7836e480d46215ec59a47c
Travis Saylor <tsaylor@Traviss-MBP.routerlogin.net>
2019-12-24 00:27:08 -0600

    Update fetch statement

--- pages/recharge.js
+++ pages/recharge.js
@@ -7,71 +7,28 @@ import fetch from 'isomorphic-unfetch'
 
 class Recharge extends React.Component{
     static async getInitialProps() {
-        var quoteRes = null;
-        var quoteData = null;
-        var quote = null;
-        var escapedQuote = null;
-        var gif = null;
-        try {
-            quoteRes = await fetch('https://michael-scott-quotes.herokuapp.com/quote');
-            quoteData = await quoteRes.json();
-            quote = quoteData.quote;
-            escapedQuote = encodeURI('michael scott ' + quote);
-        } catch (error) {
-            console.log(error);
-        }
-
-        try {
-            const gifRes = await fetch('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + escapedQuote);
-            const gifData = await gifRes.json();
-            gif = gifData.data[0].images.fixed_height_downsampled.webp
-        } catch(error) {
-            console.log(error);
-            const gifRes = await fetch('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=michael%20scott');
-            const gifData = await gifRes.json();
-            gif = gifData.data[0].images.fixed_height_downsampled.webp
-        } 
-        
-        return {quote, gif, escapedQuote}
+        const res = await fetch('https://www.reddit.com/r/ClashRoyale/comments/eer26e/an_option_to_organize_the_order_of_your_emotes.json')
+        const json = await res.json()
+        console.log(json);
+        const img = json[0].data.children[0].data.thumbnail;
+        return {img}
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            quote: this.props.quote,
-            gif: this.props.gif
+            img: this.props.img
         }
     }
 
     async componentDidMount() {
-        var quoteRes = null;
-        var quoteData = null;
-        var quote = null;
-        var escapedQuote = null;
-        var gif = null;
-        try {
-            quoteRes = await fetch('https://michael-scott-quotes.herokuapp.com/quote');
-            quoteData = await quoteRes.json();
-            quote = quoteData.quote;
-            escapedQuote = encodeURI('michael scott ' + quote);
-        } catch (error) {
-            console.log(error);
-        }
-        try {
-            const gifRes = await fetch('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + escapedQuote);
-            const gifData = await gifRes.json();
-            gif = gifData.data[0].images.fixed_height_downsampled.webp
-        } catch(error) {
-            console.log(error);
-            const gifRes = await fetch('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=michael%20scott');
-            const gifData = await gifRes.json();
-            gif = gifData.data[0].images.fixed_height_downsampled.webp
-        } finally {
-            this.setState({
-                quote: this.quote,
-                gif: this.gif
-            });
-        }
+        const res = await fetch('https://www.reddit.com/r/ClashRoyale/comments/eer26e/an_option_to_organize_the_order_of_your_emotes.json')
+        const json = await res.json()
+        
+        const img = json[0].data.children[0].data.thumbnail;
+        this.setState({
+            img: this.props.img
+        })
     }
 
     render() {
@@ -85,8 +42,7 @@ class Recharge extends React.Component{
                 <Nav />
 
                 <h1>Michael Scott Quote</h1>
-                <p>{this.state.quote}</p>
-                <img src={this.state.gif} />
+                <img src={this.state.img} />
             </div>
         )
     }
